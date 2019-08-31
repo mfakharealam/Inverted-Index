3a806a7b5040eb56d9700f4b69c37b24b97ce31c
MFA571 <fakhar571@gmail.com>
2019-08-31 20:37:06 +0500

    added term_info file containing relevant info

--- document_tokenizer.py
+++ document_tokenizer.py
@@ -29,7 +29,8 @@ class TokenizeDocument:
         stop_file.close()
         return stop_list
 
-    def file_parser(self, filename):
+    def file_parser(self, filename, pos):
+        term_dictionary = {}
         with open(os.path.join(self.corpus_dir, filename), encoding="utf-8", errors='ignore') as file:
             html_soup = BeautifulSoup(file, "html.parser")
         ignored_tags = html_soup.find_all(["script", "style"])
@@ -48,19 +49,41 @@ class TokenizeDocument:
                     stem_word = re.sub('[^a-zA-Z]+', '', stem_word)
                     if stem_word not in self.words_stop_list:
                         if stem_word not in self.words_vocabulary_list:
+                            term_dictionary[stem_word] = []
+                            term_dictionary[stem_word].append(pos)
                             self.words_list.append([stem_word, self.term_id])
                             self.words_vocabulary_list.add(stem_word)
                             term_file.write(str(self.term_id) + "\t" + stem_word + "\n")
                             self.term_id += 1
+                        elif stem_word in self.words_vocabulary_list:
+                            if stem_word not in term_dictionary:
+                                term_dictionary[stem_word] = []
+                            term_dictionary[stem_word].append(pos)
+                        pos += 1
             term_file.close()
+            return term_dictionary
+
+    def write_doc_info(self, doc_id, term_dict):
+        with open("term_info.txt", 'a', encoding='utf8', errors='ignore') as term_info_file:
+            for word in self.words_list:
+                if term_dict.get(word[0]):
+                    term_info_file.write(str(doc_id) + "\t" + str(word[1]))
+                    for pos in term_dict.get(word[0]):
+                        term_info_file.write("\t" + str(pos))
+                    term_info_file.write("\n")
+        term_info_file.close()
 
     def construct_index(self):
+        index_pos = 1  # position of term in doc
         for filename in os.listdir(self.corpus_dir):
             if os.path.isfile(os.path.join(self.corpus_dir, filename)):
                 with open("docsid.txt", 'a') as doc_file:
                     doc_file.write(str(self.doc_id) + "\t" + filename + "\n")
+                term_dictionary = self.file_parser(filename, index_pos)
+                if term_dictionary is not None:
+                    self.write_doc_info(self.doc_id, term_dictionary)
+                    term_dictionary.clear()
                 self.doc_id += 1
-                self.file_parser(filename)
 
 
 if __name__ == "__main__":
