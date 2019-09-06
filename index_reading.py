2e77514eb043c02d1e1549a957f01ec8a17b59b6
MFA571 <fakhar571@gmail.com>
2019-09-06 23:10:34 +0500

    optimized index reading using linecache module

--- index_reading.py
+++ index_reading.py
@@ -1,6 +1,8 @@
 import sys
 from nltk.stem import PorterStemmer
 import re
+import time
+import linecache
 
 
 def get_term_details(term):
@@ -19,15 +21,20 @@ def get_term_details(term):
                     break
                 i += 2  # since term name is at 2nd pos each time
             if term_id != 0:
-                while True:
-                    each_line = term_index_file.readline().split()
-                    if len(each_line) > 0:
-                        if term_id == each_line[0]:
-                            total_corpus_occurrences = each_line[1]
-                            occurrences_in_diff_docs = each_line[2]
-                            break
-                    else:
-                        break
+                read_line_no = term_id
+                the_line = linecache.getline("term_index.txt", int(read_line_no)).split()
+                term_id = the_line[0]
+                total_corpus_occurrences = the_line[1]
+                occurrences_in_diff_docs = the_line[2]
+                # while True:
+                #     each_line = term_index_file.readline().split()
+                #     if len(each_line) > 0:
+                #         if term_id == each_line[0]:
+                #             total_corpus_occurrences = each_line[1]
+                #             occurrences_in_diff_docs = each_line[2]
+                #             break
+                #     else:
+                #         break
                 print("Listing for term:", term)
                 print("Term ID:", term_id)
                 print("Number of documents containing term:", occurrences_in_diff_docs)
@@ -39,5 +46,8 @@ def get_term_details(term):
 
 
 if __name__ == '__main__':
+    start_time = time.perf_counter()
     if sys.argv[1] == '--term':
         get_term_details(sys.argv[2])
+
+    print("Total execution time: {}".format(time.perf_counter() - start_time))
