3a7fe3debde729e106abc1a5301982ddf78f7f25
MFA571 <fakhar571@gmail.com>
2019-09-09 14:33:48 +0500

    added delta encoding in sorted list version of II

--- index_reading.py
+++ index_reading.py
@@ -12,8 +12,8 @@ def get_term_details(term):
     if term is not None:
         stemmer = PorterStemmer()
         stem_word = stemmer.stem(term).lower()
-        stem_word = re.sub('[^a-zA-Z]+', ' ', stem_word)     # clear punctuation
-        with open("termsid.txt", "r", encoding="utf-8") as terms_id_file, open("term_index.txt", "r", encoding="utf-8") as term_index_file:
+        stem_word = re.sub('[^a-zA-Z]+', '', stem_word)     # clear punctuation
+        with open("termids.txt", "r", encoding="utf-8") as terms_id_file, open("term_index.txt", "r", encoding="utf-8") as term_index_file:
             term_id_doc = terms_id_file.read().split()
             for i in range(1, len(term_id_doc)):
                 if term_id_doc[i] == stem_word:
@@ -26,15 +26,6 @@ def get_term_details(term):
                 term_id = the_line[0]
                 total_corpus_occurrences = the_line[1]
                 occurrences_in_diff_docs = the_line[2]
-                # while True:
-                #     each_line = term_index_file.readline().split()
-                #     if len(each_line) > 0:
-                #         if term_id == each_line[0]:
-                #             total_corpus_occurrences = each_line[1]
-                #             occurrences_in_diff_docs = each_line[2]
-                #             break
-                #     else:
-                #         break
                 print("Listing for term:", term)
                 print("Term ID:", term_id)
                 print("Number of documents containing term:", occurrences_in_diff_docs)
@@ -49,5 +40,7 @@ if __name__ == '__main__':
     start_time = time.perf_counter()
     if sys.argv[1] == '--term':
         get_term_details(sys.argv[2])
+    else:
+        print("*** Index Reading Format is: '--term [The_term]' ***")
 
     print("Total execution time: {}".format(time.perf_counter() - start_time))
